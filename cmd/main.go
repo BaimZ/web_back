b5f7dcb36090aa0d59e1ab289160852c7cb0f466
BaimZ <go-yura@bk.ru>
2023-12-25 02:04:49 +0300

    docker connect

--- cmd/main.go
+++ cmd/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	_ "github.com/lib/pq"
 	"github.com/spf13/viper"
 	webback "github.com/zaim/web_back"
 	"github.com/zaim/web_back/pkg/handler"
@@ -15,7 +16,20 @@ func main() {
 		log.Fatalf("error while initial cfg: %s", err.Error())
 	}
 
-	repos := repository.NewRepository()
+	db, err := repository.NewPosgresDB(repository.Config{
+		Host:     viper.GetString("db.host"),
+		Port:     viper.GetString("db.port"),
+		Username: viper.GetString("db.username"),
+		Password: viper.GetString("db.password"),
+		DBName:   viper.GetString("db.dbname"),
+		SSLMode:  viper.GetString("db.sslmode"),
+	})
+
+	if err != nil {
+		log.Fatalf("failed to initialize db: %s", err.Error())
+	}
+
+	repos := repository.NewRepository(db)
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 
